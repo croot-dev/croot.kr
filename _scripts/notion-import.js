665fd94360237b347b26dc9d3738c1912a287476
importer-bot 🤖 <actions@github.com>
2023-12-03 10:48:09 +0000

    [배포] Notion 변경 사항 저장

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -1,18 +1,27 @@
 const { Client } = require("@notionhq/client");
 const { NotionToMarkdown } = require("notion-to-md");
-const moment = require("moment");
+const dayjs = require("dayjs");
 const path = require("path");
 const fs = require("fs");
 const axios = require("axios");
-// or
-// import {NotionToMarkdown} from "notion-to-md";
+
+// 노션 데이터베이스 속성명
+const PROPERTY = {
+  PUBLISH: '공개', // 타입: 체크박스(checkbox)
+  TITLE: '게시물', // 타입: 제목(plain_text)
+  CREATED_AT: '날짜', // 타입: 날짜(date)
+  TAGS: '태그' // 타입: 다중선택(multi_select)
+}
+const DEFAULT_CATEGORY_NAME = '기타'; // 카테고리 없을 시 기본으로 적용할 카테고리 명
+
+// Using dotenv in local
+if (process.env.NODE_ENV === 'local') {
+  require('dotenv').config({ path: process.env.ENV_PATH, override: true });
+}
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
-
-// passing notion client to the option
-const n2m = new NotionToMarkdown({ notionClient: notion });
 
 (async () => {
   // ensure directory exists
@@ -24,90 +33,54 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
-      property: "공개",
+      property: PROPERTY.PUBLISH,
       checkbox: {
         equals: true,
       },
     },
   });
-  for (const r of response.results) {
-    // console.log(r)
-    const id = r.id;
+  for (const { id, properties, created_time } of response.results) {
     // date
-    let date = moment(r.created_time).format("YYYY-MM-DD");
-    let pdate = r.properties?.["날짜"]?.["date"]?.["start"];
-    if (pdate) {
-      date = moment(pdate).format("YYYY-MM-DD");
-    }
+    const createDate = properties?.[PROPERTY.CREATED_AT]?.["date"]?.["start"] || null;
+    const date = dayjs(createDate? createDate : created_time).format("YYYY-MM-DD");
+    
     // title
-    let title = id;
-    let ptitle = r.properties?.["게시물"]?.["title"];
-    if (ptitle?.length > 0) {
-      title = ptitle[0]?.["plain_text"];
-    }
+    const tempTitle = properties?.[PROPERTY.TITLE]?.["title"];
+    const title = tempTitle.length > 0? tempTitle[0]?.["plain_text"] : id;
+
     // tags
-    let tags = [];
-    let ptags = r.properties?.["태그"]?.["multi_select"];
-    for (const t of ptags) {
-      const n = t?.["name"];
-      if (n) {
-        tags.push(n);
-      }
-    }
-    // categories
-    let cats = [];
-    let pcats = r.properties?.["카테고리"]?.["multi_select"];
-    for (const t of pcats) {
-      const n = t?.["name"];
-      if (n) {
-        cats.push(n);
-      }
-    }
+    const tagList = properties?.[PROPERTY.TAGS]?.["multi_select"] || [];
+    const tags = tagList.map((tag) => tag['name']);
+    const category = properties?.["카테고리"]?.["select"]?.name || DEFAULT_CATEGORY_NAME
 
     // frontmatter
-    let fmtags = "";
-    let fmcats = "";
-    if (tags.length > 0) {
-      fmtags += "\ntags: [";
-      for (const t of tags) {
-        fmtags += t + ", ";
-      }
-      fmtags += "]";
-    }
-    if (cats.length > 0) {
-      fmcats += "\ncategories: [";
-      for (const t of cats) {
-        fmcats += t + ", ";
-      }
-      fmcats += "]";
-    }
-    const fm = `---
+    const frontmatter = `---
 layout: post
 date: ${date}
-title: "${title}"${fmtags}${fmcats}
+title: "${title}"
+tags: [${tags.join(',')}]
+categories: [${category ? category : '기타'}]
 ---
 
 `;
-    const mdblocks = await n2m.pageToMarkdown(id);
-    const md = n2m.toMarkdownString(mdblocks)["parent"];
+    
+    // passing notion client to the option
+    const n2m = new NotionToMarkdown({ notionClient: notion });
+    const blocks = await n2m.pageToMarkdown(id);
+    const markdown = n2m.toMarkdownString(blocks)["parent"];
+    const fileTitle = `${date}-${title.replaceAll(" ", "-")}.md`;
 
-    const ftitle = `${date}-${title.replaceAll(" ", "-")}.md`;
-
-    let index = 0;
-    let edited_md = md.replace(
+    let imageIndex = 0;
+    const edited_markdown = markdown.replace(
       /!\[(.*?)\]\((.*?)\)/g,
-      function (match, p1, p2, p3) {
-        const dirname = path.join("assets/img", ftitle);
+      (match, p1, p2, p3) => {
+        const dirname = path.join("assets/img", fileTitle);
         if (!fs.existsSync(dirname)) {
           fs.mkdirSync(dirname, { recursive: true });
         }
-        const filename = path.join(dirname, `${index}.png`);
+        const filename = path.join(dirname, `${imageIndex}.png`);
 
-        axios({
-          method: "get",
-          url: p2,
-          responseType: "stream",
-        })
+        axios.get(p2, { responseType: "stream" })
           .then(function (response) {
             let file = fs.createWriteStream(`${filename}`);
             response.data.pipe(file);
@@ -116,15 +89,12 @@ title: "${title}"${fmtags}${fmcats}
             console.log(error);
           });
 
-        let res;
-        if (p1 === "") res = "";
-        else res = `_${p1}_`;
+        const res = (p1 === '')? '' : `_${p1}_`;
+        return `![${imageIndex++}]` + `(/${filename})` + `${res}`;
+      });
 
-        return `![${index++}]` + `(/${filename})` + `${res}`;
-        });
-
-    //writing to file
-    fs.writeFile(path.join(root, ftitle), fm + edited_md, (err) => {
+    // Write file
+    fs.writeFile(path.join(root, fileTitle), frontmatter + edited_markdown, (err) => {
       if (err) {
         console.log(err);
       }
